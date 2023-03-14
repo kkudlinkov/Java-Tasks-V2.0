806dc4b358b90b458c2cc9a1ff0d6a6b5e0dbe6b
Nikolai Kudlinkov <kkudlinkov@mail.ru>
2023-03-14 14:03:51 +0300

    FEATURE: Adding task2

--- src/task2/App.java
+++ src/task2/App.java
@@ -8,14 +8,29 @@ import java.util.stream.Stream;
 public class App {
 
     public static void main(String[] args) {
-        Human h1 = new Human("Nikolay", "Kudlinkov", LocalDate.now().minusYears(19).minusDays(7), 65);
-        Human h2 = new Human("Oleg", "Ivanovich", LocalDate.now().minusYears(20).minusDays(18), 75);
+        Human h1 = new Human("Nikolay", "Kudlinkov", LocalDate.now().minusYears(20).minusDays(7), 65);
+        Human h2 = new Human("Oleg", "Ivanovich", LocalDate.now().minusYears(25).minusDays(18), 75);
         Human h3 = new Human("Vasiliy", "Terkin", LocalDate.now().minusYears(18).minusDays(12), 67);
-        Human h4 = new Human("Nikita", "Romanovich", LocalDate.now().minusYears(22).minusDays(1), 72);
+        Human h4 = new Human("Nikita", "Romanovich", LocalDate.now().minusYears(30).minusDays(1), 72);
         Human h5 = new Human("Ekaterina", "Petrovna", LocalDate.now().minusYears(21).minusDays(22), 60);
 
+        LocalDate date = LocalDate.of(1999,2,3);
+
+
         Stream<Human> stream = Stream.of(h1, h2, h3, h4, h5);
-
-
+        List<Human> data = stream
+                .peek(a1 -> a1.setWeight(a1.getWeight() + 3))
+                .collect(Collectors.toList());
+        System.out.println(data + "\n ---");
+        data = data
+                .stream()
+                .filter(a1 -> a1.getBirthDate().isBefore(date))
+                .collect(Collectors.toList());
+        System.out.println(data + "\n ---");
+        String str = data
+                .stream()
+                .map(p -> p.getLastName())
+                .collect(Collectors.joining(" "));
+        System.out.println(str + "\n ---");
     }
 }
